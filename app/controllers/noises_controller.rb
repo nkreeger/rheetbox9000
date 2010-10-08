af8ec1c19d8c6512550ef7f2c913b371de0bed80
Nick kreeger <nick@songbirdnest.com>
2010-10-08 15:51:10 -0700

    Switching playback to mac

--- app/controllers/noises_controller.rb
+++ app/controllers/noises_controller.rb
@@ -25,7 +25,7 @@ class NoisesController < ApplicationController
 
   def play
     noise = Noise.find(params[:id])
-    system("gst123 #{noise.sound.path}") unless noise.nil?
+    system("afplay #{noise.sound.path}") unless noise.nil?
     render :text => ""
   end
 
